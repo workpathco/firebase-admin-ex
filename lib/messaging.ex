033db987829319fa78c977979ed27a1cfb4e30fe
Justin Rutherford <rutherfordjr@gmail.com>
2019-05-15 11:23:57 -0400

    inspect message

--- lib/messaging.ex
+++ lib/messaging.ex
@@ -26,6 +26,7 @@ defmodule FirebaseAdminEx.Messaging do
   """
   @spec send(String.t(), String.t(), struct()) :: tuple()
   def send(project_id, oauth_token, %Message{} = message) do
+    IO.inspect(message)
     with {:ok, message} <- Message.validate(message),
          {:ok, response} <-
            Request.request(
